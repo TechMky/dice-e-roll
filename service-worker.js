3329341954f67a833ea7c63addd040e01bd6b039
TechMky <TechMky@users.noreply.github.com>
2020-07-30 12:11:51 +0000

    deploy: 7370b130aaa1840e3571a0395c5b3c2f23df2dd9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dice-e-roll/precache-manifest.412859e9f073a6c974337f0c26ae8a63.js"
+  "/dice-e-roll/precache-manifest.f045a7011735ca4e4709c16e969a3c52.js"
 );
 
 self.addEventListener('message', (event) => {
